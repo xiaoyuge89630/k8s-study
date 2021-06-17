86c8911f0e8f0aba3734eda3c6b4f4ed14f618a5
xiaoyuge <wuyu89630@163.com>
2021-06-17 18:47:25 +0800

    add deploy.jks

--- src/main/java/com/wynn/k8sstudy/K8sStudyApplication.java
+++ src/main/java/com/wynn/k8sstudy/K8sStudyApplication.java
@@ -3,6 +3,9 @@ package com.wynn.k8sstudy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Welcome
+ */
 @SpringBootApplication
 public class K8sStudyApplication {
 
